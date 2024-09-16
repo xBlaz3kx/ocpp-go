cfb529887540ab699a9b09aa79d4c20e9ea2e697
AhmedMahmoud <am303737@gmail.com>
2024-09-16 19:27:01 +0300

    revert `CertificateHashData` to be 40 chars long instead of 20

--- ocpp1.6/types/security_extension.go
+++ ocpp1.6/types/security_extension.go
@@ -84,5 +84,5 @@ type CertificateHashData struct {
 	HashAlgorithm  HashAlgorithmType `json:"hashAlgorithm" validate:"required,hashAlgorithm"`
 	IssuerNameHash string            `json:"issuerNameHash" validate:"required,max=128"`
 	IssuerKeyHash  string            `json:"issuerKeyHash" validate:"required,max=128"`
-	SerialNumber   string            `json:"serialNumber" validate:"required,max=20"`
+	SerialNumber   string            `json:"serialNumber" validate:"required,max=40"`
 }
