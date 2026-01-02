230f3febde1bb5a8dc3a6105fefc5989cafd70a4
Blaž Dular <22869613+xBlaz3kx@users.noreply.github.com>
2026-01-02 18:02:49 +0100

    fix: only test queues concurrently (#52)

--- ocppj/queue_bench_test.go
+++ ocppj/queue_bench_test.go
@@ -2,22 +2,10 @@ package ocppj_test
 
 import (
 	"fmt"
-	"sync"
 	"testing"
 
 	"github.com/xBlaz3kx/ocpp-go/ocppj"
 )
-
-// BenchmarkFIFOClientQueue_Push benchmarks pushing elements to the queue
-func BenchmarkFIFOClientQueue_Push(b *testing.B) {
-	queue := ocppj.NewFIFOClientQueue(0) // Unlimited capacity
-	req := newMockRequest("benchmark")
-
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		_ = queue.Push(req)
-	}
-}
 
 // BenchmarkFIFOClientQueue_PushParallel benchmarks concurrent pushes
 func BenchmarkFIFOClientQueue_PushParallel(b *testing.B) {
@@ -30,22 +18,6 @@ func BenchmarkFIFOClientQueue_PushParallel(b *testing.B) {
 			_ = queue.Push(req)
 		}
 	})
-}
-
-// BenchmarkFIFOClientQueue_Pop benchmarks popping elements from the queue
-func BenchmarkFIFOClientQueue_Pop(b *testing.B) {
-	queue := ocppj.NewFIFOClientQueue(0)
-	req := newMockRequest("benchmark")
-
-	// Pre-populate queue
-	for i := 0; i < b.N; i++ {
-		_ = queue.Push(req)
-	}
-
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		_ = queue.Pop()
-	}
 }
 
 // BenchmarkFIFOClientQueue_PopParallel benchmarks concurrent pops
@@ -66,18 +38,6 @@ func BenchmarkFIFOClientQueue_PopParallel(b *testing.B) {
 	})
 }
 
-// BenchmarkFIFOClientQueue_Peek benchmarks peeking at the front element
-func BenchmarkFIFOClientQueue_Peek(b *testing.B) {
-	queue := ocppj.NewFIFOClientQueue(0)
-	req := newMockRequest("benchmark")
-	_ = queue.Push(req)
-
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		_ = queue.Peek()
-	}
-}
-
 // BenchmarkFIFOClientQueue_PeekParallel benchmarks concurrent peeks
 func BenchmarkFIFOClientQueue_PeekParallel(b *testing.B) {
 	queue := ocppj.NewFIFOClientQueue(0)
@@ -90,20 +50,6 @@ func BenchmarkFIFOClientQueue_PeekParallel(b *testing.B) {
 			_ = queue.Peek()
 		}
 	})
-}
-
-// BenchmarkFIFOClientQueue_Size benchmarks getting the queue size
-func BenchmarkFIFOClientQueue_Size(b *testing.B) {
-	queue := ocppj.NewFIFOClientQueue(0)
-	req := newMockRequest("benchmark")
-	for i := 0; i < 1000; i++ {
-		_ = queue.Push(req)
-	}
-
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		_ = queue.Size()
-	}
 }
 
 // BenchmarkFIFOClientQueue_SizeParallel benchmarks concurrent size checks
@@ -122,43 +68,6 @@ func BenchmarkFIFOClientQueue_SizeParallel(b *testing.B) {
 	})
 }
 
-// BenchmarkFIFOClientQueue_IsEmpty benchmarks checking if queue is empty
-func BenchmarkFIFOClientQueue_IsEmpty(b *testing.B) {
-	queue := ocppj.NewFIFOClientQueue(0)
-
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		_ = queue.IsEmpty()
-	}
-}
-
-// BenchmarkFIFOClientQueue_IsFull benchmarks checking if queue is full
-func BenchmarkFIFOClientQueue_IsFull(b *testing.B) {
-	capacity := 1000
-	queue := ocppj.NewFIFOClientQueue(capacity)
-	req := newMockRequest("benchmark")
-	for i := 0; i < capacity; i++ {
-		_ = queue.Push(req)
-	}
-
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		_ = queue.IsFull()
-	}
-}
-
-// BenchmarkFIFOClientQueue_PushPop benchmarks the push-pop cycle
-func BenchmarkFIFOClientQueue_PushPop(b *testing.B) {
-	queue := ocppj.NewFIFOClientQueue(0)
-	req := newMockRequest("benchmark")
-
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		_ = queue.Push(req)
-		_ = queue.Pop()
-	}
-}
-
 // BenchmarkFIFOClientQueue_PushPopParallel benchmarks concurrent push-pop operations
 func BenchmarkFIFOClientQueue_PushPopParallel(b *testing.B) {
 	queue := ocppj.NewFIFOClientQueue(0)
@@ -171,35 +80,6 @@ func BenchmarkFIFOClientQueue_PushPopParallel(b *testing.B) {
 			_ = queue.Pop()
 		}
 	})
-}
-
-// BenchmarkFIFOClientQueue_MixedOperations benchmarks mixed operations
-func BenchmarkFIFOClientQueue_MixedOperations(b *testing.B) {
-	queue := ocppj.NewFIFOClientQueue(1000)
-	req := newMockRequest("benchmark")
-
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		_ = queue.Push(req)
-		_ = queue.Peek()
-		_ = queue.Size()
-		_ = queue.IsEmpty()
-		if i%2 == 0 {
-			_ = queue.Pop()
-		}
-	}
-}
-
-// BenchmarkFIFOQueueMap_Get benchmarks getting a queue from the map
-func BenchmarkFIFOQueueMap_Get(b *testing.B) {
-	queueMap := ocppj.NewFIFOQueueMap(100)
-	clientID := "client1"
-	queueMap.GetOrCreate(clientID)
-
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		_, _ = queueMap.Get(clientID)
-	}
 }
 
 // BenchmarkFIFOQueueMap_GetParallel benchmarks concurrent gets
@@ -216,17 +96,6 @@ func BenchmarkFIFOQueueMap_GetParallel(b *testing.B) {
 	})
 }
 
-// BenchmarkFIFOQueueMap_GetOrCreate benchmarks getting or creating a queue
-func BenchmarkFIFOQueueMap_GetOrCreate(b *testing.B) {
-	queueMap := ocppj.NewFIFOQueueMap(100)
-
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		clientID := fmt.Sprintf("client%d", i%10) // Reuse 10 clients
-		_ = queueMap.GetOrCreate(clientID)
-	}
-}
-
 // BenchmarkFIFOQueueMap_GetOrCreateParallel benchmarks concurrent get-or-create operations
 func BenchmarkFIFOQueueMap_GetOrCreateParallel(b *testing.B) {
 	queueMap := ocppj.NewFIFOQueueMap(100)
@@ -240,55 +109,6 @@ func BenchmarkFIFOQueueMap_GetOrCreateParallel(b *testing.B) {
 			i++
 		}
 	})
-}
-
-// BenchmarkFIFOQueueMap_Add benchmarks adding a queue to the map
-func BenchmarkFIFOQueueMap_Add(b *testing.B) {
-	queueMap := ocppj.NewFIFOQueueMap(100)
-
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		clientID := fmt.Sprintf("client%d", i)
-		queue := ocppj.NewFIFOClientQueue(100)
-		queueMap.Add(clientID, queue)
-	}
-}
-
-// BenchmarkFIFOQueueMap_Remove benchmarks removing a queue from the map
-func BenchmarkFIFOQueueMap_Remove(b *testing.B) {
-	queueMap := ocppj.NewFIFOQueueMap(100)
-
-	// Pre-populate map
-	for i := 0; i < b.N; i++ {
-		clientID := fmt.Sprintf("client%d", i)
-		queueMap.GetOrCreate(clientID)
-	}
-
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		clientID := fmt.Sprintf("client%d", i)
-		queueMap.Remove(clientID)
-	}
-}
-
-// BenchmarkFIFOQueueMap_Size benchmarks getting total size across all queues
-func BenchmarkFIFOQueueMap_Size(b *testing.B) {
-	queueMap := ocppj.NewFIFOQueueMap(100)
-	req := newMockRequest("benchmark")
-
-	// Pre-populate with queues and elements
-	for i := 0; i < 100; i++ {
-		clientID := fmt.Sprintf("client%d", i)
-		queue := queueMap.GetOrCreate(clientID)
-		for j := 0; j < 10; j++ {
-			_ = queue.Push(req)
-		}
-	}
-
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		_ = queueMap.Size()
-	}
 }
 
 // BenchmarkFIFOQueueMap_SizeParallel benchmarks concurrent size checks
@@ -313,26 +133,6 @@ func BenchmarkFIFOQueueMap_SizeParallel(b *testing.B) {
 	})
 }
 
-// BenchmarkFIFOQueueMap_SizePerClient benchmarks getting size per client
-func BenchmarkFIFOQueueMap_SizePerClient(b *testing.B) {
-	queueMap := ocppj.NewFIFOQueueMap(100)
-	req := newMockRequest("benchmark")
-
-	// Pre-populate with queues and elements
-	for i := 0; i < 100; i++ {
-		clientID := fmt.Sprintf("client%d", i)
-		queue := queueMap.GetOrCreate(clientID)
-		for j := 0; j < 10; j++ {
-			_ = queue.Push(req)
-		}
-	}
-
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		_ = queueMap.SizePerClient()
-	}
-}
-
 // BenchmarkFIFOQueueMap_SizePerClientParallel benchmarks concurrent size-per-client checks
 func BenchmarkFIFOQueueMap_SizePerClientParallel(b *testing.B) {
 	queueMap := ocppj.NewFIFOQueueMap(100)
@@ -353,51 +153,4 @@ func BenchmarkFIFOQueueMap_SizePerClientParallel(b *testing.B) {
 			_ = queueMap.SizePerClient()
 		}
 	})
-}
-
-// BenchmarkFIFOQueueMap_MixedOperations benchmarks mixed map operations
-func BenchmarkFIFOQueueMap_MixedOperations(b *testing.B) {
-	queueMap := ocppj.NewFIFOQueueMap(100)
-	req := newMockRequest("benchmark")
-
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		clientID := fmt.Sprintf("client%d", i%10)
-		queue := queueMap.GetOrCreate(clientID)
-		_ = queue.Push(req)
-		_ = queueMap.Size()
-		if i%5 == 0 {
-			queueMap.Remove(clientID)
-		}
-	}
-}
-
-// BenchmarkFIFOQueueMap_ConcurrentClients benchmarks concurrent operations on different clients
-func BenchmarkFIFOQueueMap_ConcurrentClients(b *testing.B) {
-	queueMap := ocppj.NewFIFOQueueMap(100)
-	req := newMockRequest("benchmark")
-	numClients := 10
-
-	// Pre-create clients
-	for i := 0; i < numClients; i++ {
-		clientID := fmt.Sprintf("client%d", i)
-		queueMap.GetOrCreate(clientID)
-	}
-
-	b.ResetTimer()
-	var wg sync.WaitGroup
-	for i := 0; i < numClients; i++ {
-		wg.Add(1)
-		go func(clientID string) {
-			defer wg.Done()
-			for j := 0; j < b.N/numClients; j++ {
-				queue, _ := queueMap.Get(clientID)
-				if queue != nil {
-					_ = queue.Push(req)
-					_ = queue.Pop()
-				}
-			}
-		}(fmt.Sprintf("client%d", i))
-	}
-	wg.Wait()
 }
